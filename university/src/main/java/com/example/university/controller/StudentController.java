916ee100f22e1d8c33d7382942a2ae22e4b384f4
Youdan Zhang <gloria2612@outlook.com>
2022-12-04 19:00:53 -0500

    Add files via upload

---
+++ university/src/main/java/com/example/university/controller/StudentController.java
@@ -0,0 +1,54 @@
+package com.example.university.controller;
+
+import com.example.university.domain.CommonResponse;
+import com.example.university.domain.entity.Student;
+import com.example.university.exception.ResourceNotFoundException;
+import com.example.university.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommonResponse> findStuById(@PathVariable String id) {
+        return new ResponseEntity<>(studentService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<CommonResponse> findAll() {
+        return new ResponseEntity<>(studentService.findAll(), HttpStatus.OK);
+    }
+
+    //path: /student, method: post ,  create student
+    @PostMapping
+    public ResponseEntity<CommonResponse> insert(@RequestBody Student student) {
+        return new ResponseEntity<>(studentService.insert(student), HttpStatus.CREATED);
+    }
+
+    //TODO: /{id} , method : put,  update student
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> update( @RequestBody String student_name,@PathVariable String student_id){
+        return new ResponseEntity<>(studentService.update(student_name, student_id), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CommonResponse> handleNotFound() {
+        return new ResponseEntity<>(
+                    new CommonResponse(-1, new Date(), "resource not found"),
+                    HttpStatus.NOT_FOUND
+                );
+    }
+}
